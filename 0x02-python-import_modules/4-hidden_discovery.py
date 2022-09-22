286309d01e8534514ccfef1e56bf43a27d2cf190
yusufhaji7 <yususfhadji873@gmail.com>
2022-09-22 12:43:14 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
+    import hidden_4
+
     names = dir(hidden_4)
-    for i in names:
-        if i[:2] != '__':
-            print("{:s}".format(i))
+    for name in names:
+        if name[:2] != "__":
+            print(name)
