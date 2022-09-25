d46f103ea8151d87aebe9424b38553c1cb948aa0
yusufhaji7 <yususfhadji873@gmail.com>
2022-09-25 14:34:29 -0700

    True || False

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     list = []
     for i in my_list:
         if (i % 2) == 0:
-            list += True
+            list.append(True)
         else:
-            list += False
+            list.append(False)
     return list
