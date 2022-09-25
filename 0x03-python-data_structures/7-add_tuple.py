049ad01185c2d55962044a12ad4322f097d7098f
yusufhaji7 <yususfhadji873@gmail.com>
2022-09-25 13:37:11 -0700

    6&7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    added_one = tuple_a[0] + tuple_b[0]
-    added_two = tuple_a[1] + tuple_b[1]
-    added = added_one, added_two
-    return added
+    a = list(tuple_a)
+    b = list(tuple_b)
+    for i in range(len(a)):
+        if len(a) < 2:
+            a.append(0)
+    for i in range(len(b)):
+        if len(b) < 2:
+            b.append(0)
+    if len(a) == 0:
+        a = [0, 0]
+    elif len(b) == 0:
+        b = [0, 0]
+    Sum = (a[0] + b[0], a[1] + b[1])
+    totalSum = tuple(Sum)
+    return totalSum
