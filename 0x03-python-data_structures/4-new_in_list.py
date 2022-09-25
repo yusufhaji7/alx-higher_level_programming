eb231d127d248770195a2ab1e645298dc2457358
yusufhaji7 <yususfhadji873@gmail.com>
2022-09-25 04:35:01 -0700

    replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
     elif idx > (len(my_list) - 1):
         return my_list
     else:
-        new = my_list
-        new[idx] = elemenet
-        return new
+        previous = my_list.copy()
+        previous[idx] = element
+        return previous
