ffbaa8f367c4fafda8331007661ae6f5310f6405
yusufhaji7 <yususfhadji873@gmail.com>
2022-09-25 04:50:45 -0700

    C me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_string = ""
     for i in my_string:
         if i == 'c' or i == 'C':
-            i == ""
+            i = ""
         new_string += i
     return new_string
