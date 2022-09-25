add9f21f8502d2de0c27653abbf3c5b31d0161a8
yusufhaji7 <yususfhadji873@gmail.com>
2022-09-25 05:07:06 -0700

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix) - 1):
+        for j in range(len(matrix)):
+            if j == len(matrix):
+                print(matrix[i][j])
+            else:
+                print(matrix[i][j], end=" ")
+        print()
