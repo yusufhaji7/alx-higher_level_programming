6799a119279844984b7a3f306f7712a22870802c
yusufhaji7 <yususfhadji873@gmail.com>
2022-09-29 05:15:19 -0700

    fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for k, v in sorted(a_dictionary.items()):
-        print("{}: {}".format(k, v * 2))
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
