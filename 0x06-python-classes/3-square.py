f152d1738b597a2e275eb05b0ddb2c77bcc32708
yusufhaji7 <yususfhadji873@gmail.com>
2022-10-11 07:06:30 -0700

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,7 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """initialize a size"""
-        if isinstance(size, int) == False:
+        if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
