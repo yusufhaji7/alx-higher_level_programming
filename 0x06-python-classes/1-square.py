5a7614b4222d9bb78ce8a9bb40405dcd3e695b6d
yusufhaji7 <yususfhadji873@gmail.com>
2022-10-11 06:09:26 -0700

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square class
+    """
+    def __init__(self, size):
+        """
+        instantiation with size
+        """
+        self.__size = size
