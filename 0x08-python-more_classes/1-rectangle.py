259452296d0dbb01fe427fd753cda795c5b49264
yusufhaji7 <yususfhadji873@gmail.com>
2022-10-19 06:17:11 -0700

    fix task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,29 +12,33 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """inistantiate width and height"""
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
+        self.height = height
+        self.width = width
 
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
-
+    @property
     def width(self):
         """getter method to return width instance"""
         return self.__width
+
+    @width.setter
     def width(self, value):
         """setter method to set width to value"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
+
+    @propert    
     def height(self):
         """getter method to return height instance"""
         return self.__height
+
+    @height.setter
     def height(self, value):
         """setter method to set height to value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
