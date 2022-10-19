08d8dae022cec518302467637b0be521be97cda4
yusufhaji7 <yususfhadji873@gmail.com>
2022-10-19 08:17:46 -0700

    pycodestyle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
         getter and setter methods for height and width
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """inistantiate width and height"""
         self.height = height
