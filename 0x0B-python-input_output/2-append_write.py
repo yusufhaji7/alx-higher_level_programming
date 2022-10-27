7ce57756ae44696b920a94c4c221e3744eee8baf
yusufhaji7 <yususfhadji873@gmail.com>
2022-10-26 18:45:52 -0700

    the rest

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+declares the function "append_write"
+"""
+
+
+def append_write(filename="", text=""):
+    """returns the number of chars appended to "filename" from "text" """
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
