2838cc6f0bacfaba51f7c2ba7f4b8b34911e4d00
yusufhaji7 <yususfhadji873@gmail.com>
2022-10-31 05:43:58 -0700

    update rectangle

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-""" 2-main """
+""" 6-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    try:
-        Rectangle(10, "2")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    r1 = Rectangle(2, 3, 2, 2)
+    r1.display()
 
-    try:
-        r = Rectangle(10, 2)
-        r.width = -10
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    print("---")
 
-    try:
-        r = Rectangle(10, 2)
-        r.x = {}
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        Rectangle(10, 2, 3, -1)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    r2 = Rectangle(3, 2, 1, 0)
+    r2.display()
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,35 +18,39 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """constructor to initialize width, height, x and y"""
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
 
     @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
     @property
-    def width(self):
-        return self.__width
+    def height(self):
+        return self.__height
     
     @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value <=0:
+        if value <=0:
             raise ValueError("height must be > 0")
         self.__height = value
 
     @property
-    def height(self):
-        return self.__height
+    def x(self):
+        return self.__x
 
     @x.setter
     def x(self, value):
@@ -55,10 +59,10 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
-
+    
     @property
-    def x(self):
-        return self.__x
+    def y(self):
+        return self.__y
 
     @y.setter
     def y(self, value):
@@ -68,9 +72,12 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    @property
-    def y(self):
-        return self.__y
-
     def area(self):
         return self.__width * self.__height
+
+    def display(self):
+        for i in range(self.__height):
+            print(("\n" * self.__y) + "\n".join(((" " * self.__x) + ("#" * self.__width))))
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
