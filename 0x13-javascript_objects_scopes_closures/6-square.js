4d58a50c410441d3ea8abcbd0ee500f3497bf834
yusufhadji7 <yusufhadji7@gmail.com>
2022-12-29 12:00:58 +0000

    6th task finished

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
-      if (c === 'C') {
-        console.log('C'.repeat(this.size));
-      } else {
-        console.log('X'.repeat(this.size));
-      }
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
