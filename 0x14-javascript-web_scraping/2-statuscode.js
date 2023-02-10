1b05a92b568f7216c3155e013d21063aaabad052
yusufhaji7 <yususfhadji873@gmail.com>
2023-02-10 14:12:43 -0800

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-require('request').get(process.argv[2], (err, response) => {
-	if (err){
-		console.log(err);
-	}else{
-		console.log('code: ' + response.statuscode)}
+require('request').get(process.argv[2], function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
