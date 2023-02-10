e8a1df86dc8c071c04a0dec121f53a99fe1538cd
yusufhaji7 <yususfhadji873@gmail.com>
2023-02-10 14:35:19 -0800

    sixth task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+require('request').get(process.argv[2], function (err, r, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    require('fs').writeFile(process.argv[3], body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
